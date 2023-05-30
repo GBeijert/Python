4935c61aa7bc3f6547620a3effbd6c61c84947fa
Gerben Beijert <G.Beijert@chipsoft.com>
2023-05-30 13:54:55 +0200

    First revision holy handgrenade

---
+++ Source/Holy handgrenade.py
@@ -0,0 +1,26 @@
+import random
+
+class Chest():
+    def __init__(self, contains_holy_handgrenade):
+        self.contains_holy_handgrenade = contains_holy_handgrenade  
+
+#create list and append chests
+def create_chests():
+    pass
+    
+chests = create_chests()    
+    
+#shuffle two random chests, show the player what chests are shuffled!
+def shuffle_chests():   
+    pass
+
+#Ask the user to guess
+def ask_user_for_input():
+    pass
+
+#define game logic here.
+def play_game():
+    pass
+
+play_game()
+    
